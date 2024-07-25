0fe018d7ad43ebe2074210212223eb13db2a0430
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-07-25 18:48:49 +0530

    Create Move Zeroes.java

---
+++ Arrays/Move Zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        int j=i+1;
+        while(i<nums.length && j < nums.length){
+            if(nums[i]!=0){ i++;j++;}
+            else if(nums[i] == 0 && nums[j]!=0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j] =temp;
+            }
+            else j++;
+        }
+    }
+}
