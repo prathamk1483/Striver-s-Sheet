702a3b27bc8ce0d8d8392f899960886128298a6c
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-07-25 17:52:09 +0530

    Create  Max Consecutive Ones III.java

---
+++ Sliding Window & Two Pointers/Max Consecutive Ones III.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int i=0,j=0,ans=0,z=0;
+        while(j<nums.length){
+            if(nums[j] == 0) z++;
+            while(z>k){
+                if(nums[i] == 0)z--;
+                i++;
+            }
+            ans = Math.max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+}
