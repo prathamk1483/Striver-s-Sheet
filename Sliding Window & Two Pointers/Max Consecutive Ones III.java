743e7eff0f3d81ec2b76987db97de6dd45c9071e
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-07-25 18:27:37 +0530

    Update Max Consecutive Ones III.java

--- Sliding Window & Two Pointers/Max Consecutive Ones III.java
+++ Sliding Window & Two Pointers/Max Consecutive Ones III.java
@@ -13,3 +13,23 @@ class Solution {
         return ans;
     }
 }
+
+
+// OR
+
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int i=0,j=0,ans=0,z=0;
+        while(j<nums.length){
+            if(nums[j] == 0) z++;
+            if(z>k){
+                if(nums[i] == 0)z--;
+                i++;
+            }
+            ans = Math.max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+}
