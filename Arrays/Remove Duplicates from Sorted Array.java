cbeab5dd4c28ce066bb5c6b15918376b1da8dead
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-07-25 18:45:56 +0530

    Create Remove Duplicates from Sorted Array.java

---
+++ Arrays/Remove Duplicates from Sorted Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1]) continue;
+            else{
+                nums[j++]=nums[i];
+            }
+        }
+        return j;
+    }
+}
