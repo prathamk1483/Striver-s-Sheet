baec51a68e8a0a9a00263487560de0cff9156261
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-10-28 17:28:19 +0530

    Create Minimum Number of Days to Make m Bouquets.java

---
+++ Binary Search/Minimum Number of Days to Make m Bouquets.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int possible(int[] arr, int day, int m, int k)
+    {
+        int count=0;
+        int bday=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<=day)
+                count++;
+            else{
+                bday=bday+(count/k);
+                count=0;
+            }
+        }
+         bday=bday+(count/k);
+         return bday;   
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n=bloomDay.length;
+        long var=(long)m*k;
+        if(var>n) return -1;
+        int ans=-1;
+        int low=Integer.MAX_VALUE;
+        int high=Integer.MIN_VALUE;
+        for(int ele:bloomDay)
+        {
+            low=Math.min(low,ele);
+            high=Math.max(high,ele);
+        }
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int bday=possible(bloomDay,mid,m,k);
+            if(bday>=m)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
